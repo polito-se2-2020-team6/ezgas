5b6132f21113921e930adbb8b02fca176442a68e
vina97 <vinai@hotmail.it>
2020-05-04 09:46:55 +0200

    feat(repo): Add findall method to get all gas stations

--- src/main/java/it/polito/ezgas/repository/GasStationRepository.java
+++ src/main/java/it/polito/ezgas/repository/GasStationRepository.java
@@ -1,9 +1,13 @@
 package it.polito.ezgas.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import it.polito.ezgas.entity.GasStation;
 
 public interface GasStationRepository extends CrudRepository<GasStation, Integer> {
-
+	@Override
+	  List <GasStation> findAll();
+	 
 }
