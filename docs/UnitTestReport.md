07f88ee7328637978e0351e5765e6cf4b87a8a19
Corrado Vecchio <corradovecchio@iMac-di-Corrado-Vecchio.local>
2020-05-13 18:48:09 +0200

    test: add tests for geoPointDistance method
    update UnitTestReport
    add geoPointDistanceEclemma.png to images/Tests

--- docs/UnitTestReport.md
+++ docs/UnitTestReport.md
@@ -37,7 +37,7 @@ Version: 0.1
 **Predicates for method isGasolineTypeValid:**
 
 |    Criteria     |      Predicate      |
-|:---------------:|:-------------------:|
+| :-------------: | :-----------------: |
 |  String Length  |          0          |
 |                 |         >0          |
 | Correct Content |    Gasoline type    |
@@ -47,13 +47,13 @@ Version: 0.1
 **Boundaries**:
 
 |    Criteria     | Boundary values |
-|:---------------:|:---------------:|
+| :-------------: | :-------------: |
 | Correct Content |      null       |
 
 **Combination of predicates**:
 
 | String Length |   Correct Content   | Valid / Invalid | Description of the test case |      JUnit test case       |
-|:-------------:|:-------------------:|:---------------:|:----------------------------:|:--------------------------:|
+| :-----------: | :-----------------: | :-------------: | :--------------------------: | :------------------------: |
 |       0       |          -          |     Invalid     |    Input string is empty     | testIsGasolineTypeValid1() |
 |      > 0      | Not a gasoline type |     Invalid     |    Input string is "ciao"    | testIsGasolineTypeValid2() |
 |      > 0      |    Gasoline Type    |      Valid      |  Input string is "Methane"   | testIsGasolineTypeValid3() |
@@ -62,10 +62,52 @@ Version: 0.1
  ### Class GasStationServiceimpl - method geoPointDistance
 
 **Criteria for method geoPointDistance:**	
+-Correct latitude
+-Correct longitude 
 
+**Predicates for method geoPointDistance:**
 
+|      Criteria      |    Predicate     |
+| :----------------: | :--------------: |
+| Correct latitude1  |  -90<`lat1`<=90  |
+| Correct longitude1 | -180<`lon1`<=180 |
+| Correct latitude2  |  -90<`lat2`<=90  |
+| Correct longitude2 | -180<`lon2`<=180 |
 
+**Boundaries**:
 
+|      Criteria      |       Boundary values       |
+| :----------------: | :-------------------------: |
+| Correct latitude1  | -90 + Double.MIN_INCREMENT  |
+|                    |  90 - Double.MIN_INCREMENT  |
+|                    |             90              |
+| Correct longitude1 | -180 + Double.MIN_INCREMENT |
+|                    | 180 - Double.MIN_INCREMENT  |
+|                    |             180             |
+| Correct latitude2  | -90 + Double.MIN_INCREMENT  |
+|                    |  90 - Double.MIN_INCREMENT  |
+|                    |             90              |
+| Correct longitude2 | -180 + Double.MIN_INCREMENT |
+|                    | 180 - Double.MIN_INCREMENT  |
+|                    |             180             |
+
+**Combination of predicates**:
+
+|     Correct Latitude1      |     Correct Longitude1      |     Correct Latitude2      |     Correct Longitude2      | Valid / Invalid |            Description of the test case            |     JUnit test case      |
+| :------------------------: | :-------------------------: | :------------------------: | :-------------------------: | :-------------: | :------------------------------------------------: | :----------------------: |
+|       -90<`lat1`<=90       |      -180<`lon1`<=180       |       -90<`lat2`<=90       |      -180<`lon2`<=180       | Valid(10051.81) |             (-30.10;-100.2;-40.6;20.3)             | testGeoPointDistance1()  |
+| -90 + Double.MIN_INCREMENT |      -180<`lon1`<=180       |       -90<`lat2`<=90       |      -180<`lon2`<=180       | Valid(12278.67) |  (-90 + Double.MIN_INCREMENT;-100.45;20.43;61.6)   | testGeoPointDistance2()  |
+| 90 - Double.MIN_INCREMENT  |      -180<`lon1`<=180       |       -90<`lat2`<=90       |      -180<`lon2`<=180       | Valid(12853.52) |   (90 - Double.MIN_INCREMENT;65.21;-25.6;120.34)   | testGeoPointDistance3()  |
+|             90             |      -180<`lon1`<=180       |       -90<`lat2`<=90       |      -180<`lon2`<=180       | Valid(12975.82) |               (90;89.90;-26.7;55.7)                | testGeoPointDistance4()  |
+|       -90<`lat1`<=90       | -180 + Double.MIN_INCREMENT |       -90<`lat2`<=90       |      -180<`lon2`<=180       | Valid(11129.19) |  (-30.4;-180 + Double.MIN_INCREMENT;36.43;-100.4)  | testGeoPointDistance5()  |
+|       -90<`lat1`<=90       | 180 - Double.MIN_INCREMENT  |       -90<`lat2`<=90       |      -180<`lon2`<=180       | Valid(11587.9)  | (29.09;180 - Double.MIN_INCREMENT;-50.34;-103.34)  | testGeoPointDistance6()  |
+|       -90<`lat1`<=90       |             180             |       -90<`lat2`<=90       |      -180<`lon2`<=180       | Valid(7291.82)  |             (-45.54;180;-21.43;103.54)             | testGeoPointDistance7()  |
+|       -90<`lat1`<=90       |      -180<`lon1`<=180       | -90 + Double.MIN_INCREMENT |       180<`lon2`<=180       | Valid(12477.69) |  (22.22;-102.34;-90 + Double.MIN_INCREMENT;27.76)  | testGeoPointDistance8()  |
+|       -90<`lat1`<=90       |      -180<`lon1`<=180       | 90 - Double.MIN_INCREMENT  |       180<`lon2`<=180       | Valid(18986.73) | (-80.76;-170.54;90 - Double.MIN_INCREMENT;102.54)  | testGeoPointDistance9()  |
+|       -90<`lat1`<=90       |      -180<`lon1`<=180       |             90             |       180<`lon2`<=180       | Valid(14850.48) |              (-43.56;109.34;90;99.09)              | testGeoPointDistance10() |
+|       -90<`lat1`<=90       |      -180<`lon1`<=180       |       -90<`lat2`<=90       | -180 + Double.MIN_INCREMENT |  Valid(6366.8)  | (-30.54;103.54;-76.76;-180 + Double.MIN_INCREMENT) | testGeoPointDistance11() |
+|       -90<`lat1`<=90       |      -180<`lon1`<=180       |       -90<`lat2`<=90       | 180 - Double.MIN_INCREMENT  | Valid(6510.84)  |   (31.67;66.78;89.43;180 - Double.MIN_INCREMENT)   | testGeoPointDistance12() |
+|       -90<`lat1`<=90       |      -180<`lon1`<=180       |       -90<`lat2`<=90       |             180             | Valid(8846.99)  |              (55.76;-59.76;30.88;180)              | testGeoPointDistance13() |
 
  ### Class GasStationServiceimpl - method latLonCorrect
 
@@ -79,7 +121,7 @@ Version: 0.1
 
 
 |      Unit name      |      JUnit test case       |
-|:-------------------:|:--------------------------:|
+| :-----------------: | :------------------------: |
 | isGasolineTypeValid | testIsGasolineTypeValid1() |
 |                     | testIsGasolineTypeValid2() |
 |                     | testIsGasolineTypeValid3() |
@@ -103,7 +145,7 @@ Version: 0.1
     developed to cover zero, one or multiple iterations >
 
 | Unit name | Loop rows | Number of iterations | JUnit test case |
-|-----------|-----------|----------------------|-----------------|
+| --------- | --------- | -------------------- | --------------- |
 |           |           |                      |                 |
 |           |           |                      |                 |
 |           |           |                      |                 |
--- src/test/java/it/polito/ezgas/service/impl/TestGasStationServiceimpl.java
+++ src/test/java/it/polito/ezgas/service/impl/TestGasStationServiceimpl.java
@@ -1,5 +1,6 @@
 package it.polito.ezgas.service.impl;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -132,4 +133,160 @@ public class TestGasStationServiceimpl {
 			fail("Method not found");
 		}
 	}
+	
+	@Test
+	public void testGeoPointDistance1(){
+		try {
+			Method example = GasStationServiceimpl.class.getDeclaredMethod("geoPointDistance", double.class, double.class, double.class, double.class);
+			example.setAccessible(true);			
+			double res = (double) example.invoke(new GasStationServiceimpl(null, null), -30.10,-100.2,-40.6,20.3);
+			assertEquals(10051.81,res,1);
+		}catch(NoSuchMethodException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			fail("Method not found");
+		}
+	}	
+	@Test
+	public void testGeoPointDistance2(){
+		try {
+			Method example = GasStationServiceimpl.class.getDeclaredMethod("geoPointDistance", double.class, double.class, double.class, double.class);
+			example.setAccessible(true);
+			
+			double res = (double) example.invoke(new GasStationServiceimpl(null, null), Math.nextAfter(-90,Double.POSITIVE_INFINITY),-100.4,20.43,61.6);
+			assertEquals(12278.67,res,1);
+		}catch(NoSuchMethodException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			fail("Method not found");
+		}
+	}	
+	@Test
+	public void testGeoPointDistance3(){
+		try {
+			Method example = GasStationServiceimpl.class.getDeclaredMethod("geoPointDistance", double.class, double.class, double.class, double.class);
+			example.setAccessible(true);
+			
+			double res = (double) example.invoke(new GasStationServiceimpl(null, null), Math.nextAfter(90,Double.NEGATIVE_INFINITY),65.21,-25.6,120.34);
+			assertEquals(12853.52,res,1);
+		}catch(NoSuchMethodException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			fail("Method not found");
+		}
+	}	
+	@Test
+	public void testGeoPointDistance4(){
+		try {
+			Method example = GasStationServiceimpl.class.getDeclaredMethod("geoPointDistance", double.class, double.class, double.class, double.class);
+			example.setAccessible(true);
+			
+			double res = (double) example.invoke(new GasStationServiceimpl(null, null), 90,89.90,-26.7,55.7);
+			assertEquals(12975.82,res,1);
+		}catch(NoSuchMethodException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			fail("Method not found");
+		}
+	}	
+	@Test
+	public void testGeoPointDistance5(){
+		try {
+			Method example = GasStationServiceimpl.class.getDeclaredMethod("geoPointDistance", double.class, double.class, double.class, double.class);
+			example.setAccessible(true);
+			
+			double res = (double) example.invoke(new GasStationServiceimpl(null, null), -30.4,Math.nextAfter(-180,Double.POSITIVE_INFINITY),36.43,-100.4);
+			assertEquals(11129.19,res,1);
+		}catch(NoSuchMethodException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			fail("Method not found");
+		}
+	}	
+	@Test
+	public void testGeoPointDistance6(){
+		try {
+			Method example = GasStationServiceimpl.class.getDeclaredMethod("geoPointDistance", double.class, double.class, double.class, double.class);
+			example.setAccessible(true);
+			
+			double res = (double) example.invoke(new GasStationServiceimpl(null, null), 29.09,Math.nextAfter(180,Double.NEGATIVE_INFINITY),-50.34,-103.34);
+			assertEquals(11587.9,res,1);
+		}catch(NoSuchMethodException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			fail("Method not found");
+		}
+	}
+	@Test
+	public void testGeoPointDistance7(){
+		try {
+			Method example = GasStationServiceimpl.class.getDeclaredMethod("geoPointDistance", double.class, double.class, double.class, double.class);
+			example.setAccessible(true);
+			
+			double res = (double) example.invoke(new GasStationServiceimpl(null, null), -45.54,180,-21.43,103.54);
+			assertEquals(7291.82,res,1);
+		}catch(NoSuchMethodException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			fail("Method not found");
+		}	
+	}
+	@Test
+	public void testGeoPointDistance8(){
+		try {
+			Method example = GasStationServiceimpl.class.getDeclaredMethod("geoPointDistance", double.class, double.class, double.class, double.class);
+			example.setAccessible(true);
+			
+			double res = (double) example.invoke(new GasStationServiceimpl(null, null), 22.22,-102.34,Math.nextAfter(-90,Double.POSITIVE_INFINITY),27.76);
+			assertEquals(12477.69,res,1);
+		}catch(NoSuchMethodException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			fail("Method not found");
+		}							
+	}	
+	@Test
+	public void testGeoPointDistance9(){
+		try {
+			Method example = GasStationServiceimpl.class.getDeclaredMethod("geoPointDistance", double.class, double.class, double.class, double.class);
+			example.setAccessible(true);
+			
+			double res = (double) example.invoke(new GasStationServiceimpl(null, null), -80.76,-170.54,Math.nextAfter(90,Double.NEGATIVE_INFINITY),102.54);
+			assertEquals(18986.73,res,1);
+		}catch(NoSuchMethodException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			fail("Method not found");
+		}	
+	}
+	@Test
+	public void testGeoPointDistance10(){
+		try {
+			Method example = GasStationServiceimpl.class.getDeclaredMethod("geoPointDistance", double.class, double.class, double.class, double.class);
+			example.setAccessible(true);
+			
+			double res = (double) example.invoke(new GasStationServiceimpl(null, null), -43.56,109.34,90,99.09);
+			assertEquals(14850.48,res,1);
+		}catch(NoSuchMethodException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			fail("Method not found");
+		}	
+	}
+	@Test
+	public void testGeoPointDistance11(){
+		try {
+			Method example = GasStationServiceimpl.class.getDeclaredMethod("geoPointDistance", double.class, double.class, double.class, double.class);
+			example.setAccessible(true);
+			
+			double res = (double) example.invoke(new GasStationServiceimpl(null, null), -30.54,103.54,-76.76,Math.nextAfter(-180,Double.POSITIVE_INFINITY));
+			assertEquals(6366.8,res,1);
+		}catch(NoSuchMethodException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			fail("Method not found");
+		}	
+	}	
+	@Test
+	public void testGeoPointDistance12(){
+		try {
+			Method example = GasStationServiceimpl.class.getDeclaredMethod("geoPointDistance", double.class, double.class, double.class, double.class);
+			example.setAccessible(true);
+			
+			double res = (double) example.invoke(new GasStationServiceimpl(null, null), 31.67,66.78,89.43,Math.nextAfter(180,Double.NEGATIVE_INFINITY));
+			assertEquals(6510.84,res,1);
+		}catch(NoSuchMethodException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			fail("Method not found");
+		}	
+	}
+	@Test
+	public void testGeoPointDistance13(){
+		try {
+			Method example = GasStationServiceimpl.class.getDeclaredMethod("geoPointDistance", double.class, double.class, double.class, double.class);
+			example.setAccessible(true);
+			
+			double res = (double) example.invoke(new GasStationServiceimpl(null, null), 55.76,-59.76,30.88,180);
+			assertEquals(8846.99,res,1);
+		}catch(NoSuchMethodException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			fail("Method not found");
+		}	
+	}	
 }
