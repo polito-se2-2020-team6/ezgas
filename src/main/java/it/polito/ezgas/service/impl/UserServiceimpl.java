cb86ed8624cf06aea07efd241973084395961eb2
Alessandro Borione <leax_xiv@me.com>
2020-05-19 19:08:39 +0200

    fix: saveUser
    check for impossible scenario

--- src/main/java/it/polito/ezgas/service/impl/UserServiceimpl.java
+++ src/main/java/it/polito/ezgas/service/impl/UserServiceimpl.java
@@ -46,15 +46,18 @@ public class UserServiceimpl implements UserService {
 	@Override
 	public UserDto saveUser(UserDto userDto) {
 		User user2 = repository.findByEmail(userDto.getEmail());
-		// CASE SAVE:
-		if (userDto.getUserId()==null) { 
-			if (user2 != null) //account with same mail already existing -> reject
+		
+		// User not registered, but ID set, IMPOSSIBLE
+		if(user2 == null && userDto.getUserId() != null) {
+			return null;
+		} else if (userDto.getUserId()==null) {	// CASE SAVE: 
+			if (user2 != null) // account with same mail already existing -> reject
 				return null;
-		}
-		else { // CASE UPDATE:
+		} else {	// CASE UPDATE:
 			if ((user2 != null) && (user2.getUserId() != userDto.getUserId()))
 				return null; //other user with new email -> reject
 		}
+		
 		User user = UserMapper.toUser(userDto);
 		user=repository.save(user);
 		return UserMapper.toUserDto(user);
